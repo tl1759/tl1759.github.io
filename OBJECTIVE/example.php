9d2b69f3e318fd7ec1bd20f4ec6c194de7a48802
LiwenTian <tl1759@users.noreply.github.com>
2017-06-19 10:13:42 -0400

    change to my file

--- OBJECTIVE/example.php
+++ OBJECTIVE/example.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 require_once 'excel_reader2.php';
-$data = new Spreadsheet_Excel_Reader("example.xls");
+$data = new Spreadsheet_Excel_Reader("Places.xlsx");
 ?>
 <html>
 <head>
